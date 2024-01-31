19195b99a9fe57f512390cc85137800e75aee268
Ines-Oubabas <inesoubabas56@gmail.com>
2024-01-31 20:44:29 +0100

    test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx in range(0, len(new_list)):
-        new_list[idx] = element
-        return new_list
+    new_list[idx] = element
+    return new_list
