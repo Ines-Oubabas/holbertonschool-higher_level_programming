1fecc8935002a97157f802ecaec6f91086690080
Ines-Oubabas <inesoubabas56@gmail.com>
2024-02-08 12:20:14 +0100

    testone and two

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Defines a square."""
+"""
+Define a square class
+"""
 
 
 class Square:
-    """Represents a square."""
+    """
+    define attribute of square
+    """
 
     def __init__(self, size=0):
-        """Initializes a new Square instance.
-
+        """
+        initialize a square
         Args:
-            size (int): The size of the square.
+            size (int, optional): _description_. Defaults to 0.
+        Raises:
+                TypeError: size must be an integer
+                ValueError: size must be >=0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
