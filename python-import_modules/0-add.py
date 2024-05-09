154185ba9c80191deda55602d37b504036915579
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-09 22:09:30 +0200

    test0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
-
-def main():
+if __name__ == "__main__":
+    """
+    Prints the result of the addition between two numbers
+    """
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a, b)}")
-
-
-if __name__ == "__main__":
-    main()
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
