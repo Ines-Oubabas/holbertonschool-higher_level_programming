6adc2182dc0d019787e1f05541e5e8e600a2151d
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-14 12:19:50 +0200

    test5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-     new_str = ""
-     for c in my_string:
-         if c != 'c' and c != 'c':
-             new_str += c
-     return new_str
+    new_str = ""
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_str += c
+    return new_str
