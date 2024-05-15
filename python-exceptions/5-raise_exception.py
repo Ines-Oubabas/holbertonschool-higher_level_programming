4e191667aa66eb6035e37bb74b928ec6f24f2b0a
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-15 18:08:43 +0200

    test5

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def raise_exception():
-    """Raises a TypeError with a custom message."""
-    raise TypeError("This is a custom type error message.")
-
-
-try:
-    raise_exception()
-except TypeError as e:
-    print("Caught an exception:", e)
+    """Raises a TypeError exception."""
+    raise TypeError("An error of type TypeError occurred")
