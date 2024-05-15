cdd8559cbf7030bdcd3b03a6e4817dce3a45d4da
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-15 17:51:51 +0200

    fixed

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -2,6 +2,8 @@
 def raise_exception():
     """Raises a TypeError with a custom message."""
     raise TypeError("This is a custom type error message.")
+
+
 try:
     raise_exception()
 except TypeError as e:
