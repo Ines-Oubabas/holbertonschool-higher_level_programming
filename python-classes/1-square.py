6aeed0c7849e37eea7fc18f99bed9a1cfa45dcd3
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-17 12:01:09 +0200

    test

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
 class Square:
-    def __init(self, size):
+    """
+    define attribute of square
+    """
+
+    def __init__(self, size):
         self.__size = size
