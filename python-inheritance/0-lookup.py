fdd471918280fb687de21c0a6d3a414392b5530f
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-22 22:04:42 +0200

    c bon?

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+"""
+Lookup order in python
+"""
+
+
 def lookup(obj):
     """
-    Returns the list of available attributes and methods of an object.
-
-    Args:
-        obj: The object to inspect.
-
-    Returns:
-        List of strings, each representing an attribute or method of the object
-        """
+    Lookup order
+    """
 
     return dir(obj)
