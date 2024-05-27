e893663a55e23de47f4785f0ebc99d99208c0834
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-27 14:25:31 +0200

    harimna

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Mdule for file reading operations."""
+
+
 def read_file(filename=""):
+    """read a file and print its content.
+
+    Args:
+            filname (str, optional): file to read and print.
+            Defaults to "".
+    """
     with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content, end='')
+        read_file = file.read()
+        print(read_file, end='')
