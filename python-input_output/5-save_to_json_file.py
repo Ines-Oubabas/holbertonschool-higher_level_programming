c04858c478f6205ca2cfc799af83cf6a8e3828a9
Ines-Oubabas <inesoubabas56@gmail.com>
2024-05-27 17:31:36 +0200

    test

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""JSON file saving module."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Serializes an object to a JSON string and writes it to a file.
+
+    Args:
+        my_obj: The Python object to serialize.
+        filename (str): The name of the file where the JSON string
+        will be saved.
+
+    """
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
